796d8421b1b8f49bffe84eed6911c32877c20e70
amirz132 <46897821+amirz132@users.noreply.github.com>
2020-10-09 09:14:11 +0600

    Add files via upload

---
+++ scraper_execute.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct  9 07:38:37 2020
+
+@author: pc
+"""
+
+import web_scraper_glassdoor as gs
+import pandas as pd
+
+path = "C:/Users/pc/Documents/ds_intern_salary_proj/chromedriver_win32/chromedriver.exe"
+
+df = gs.get_jobs('data scientist intern',15,False,path,15)
