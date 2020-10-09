ca23c25c4bb4ba7f4df13de77790cdbdc9669483
amirz132 <46897821+amirz132@users.noreply.github.com>
2020-10-09 11:42:31 +0600

    uploaded data and data collection script

--- scraper_execute.py
+++ scraper_execute.py
@@ -10,4 +10,6 @@ import pandas as pd
 
 path = "C:/Users/pc/Documents/ds_intern_salary_proj/chromedriver_win32/chromedriver.exe"
 
-df = gs.get_jobs('data scientist intern',15,False,path,15)
+df = gs.get_jobs('data scientist intern',200,False,path,15)
+
+df.to_csv('ds_jobs.csv',index = False)
